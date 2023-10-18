902b8c19257d115fd0e99af598defd5c73fea72f
John James <john.james.ai.studio@gmail.com>
2023-10-18 07:16:14 -0400

    artifact removal, pectoral muscle removal

---
+++ bcd/data/prep/image/enhance.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# ================================================================================================ #
+# Project    : Deep Learning for Breast Cancer Detection                                           #
+# Version    : 0.1.0                                                                               #
+# Python     : 3.10.12                                                                             #
+# Filename   : /bcd/data/prep/image/enhance.py                                                     #
+# ------------------------------------------------------------------------------------------------ #
+# Author     : John James                                                                          #
+# Email      : john.james.ai.studio@gmail.com                                                      #
+# URL        : https://github.com/john-james-ai/BreastCancerDetection                              #
+# ------------------------------------------------------------------------------------------------ #
+# Created    : Tuesday October 17th 2023 07:15:25 am                                               #
+# Modified   : Tuesday October 17th 2023 07:15:26 am                                               #
+# ------------------------------------------------------------------------------------------------ #
+# License    : MIT License                                                                         #
+# Copyright  : (c) 2023 John James                                                                 #
+# ================================================================================================ #
